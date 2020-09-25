8e8232b24da151002be2123d67ad22c1e03b48f5
jerrygoyal <1gouravgg@gmail.com>
2020-09-26 00:55:48 +0530

    added more meta

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,15 +1,32 @@
 import Head from "next/head";
 
 export default function Header(Props: { title: string }): JSX.Element {
+  const { title } = Props;
+  const desc =
+    "I'm a tech founder, full-stack developer, and a fun guy to hang around with.";
+  const site = "https://gourav.io";
   return (
     <Head>
-      <title>{Props.title}</title>
+      <title>{title}</title>
+      <meta name="author" content={title} />
       <meta charSet="UTF-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <meta
-        name="description"
-        content="I'm a tech founder, full-stack developer, and a fun guy to hang around with."
-      />
+      <meta name="title" content={title} />
+      <meta name="description" content={desc} />
+
+      {/* <!-- Open Graph / Facebook --> */}
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content={site} />
+      <meta property="og:title" content={title} />
+      <meta property="og:description" content={desc} />
+      <meta property="og:image" content="" />
+
+      {/* <!-- Twitter --> */}
+      <meta property="twitter:card" content="summary_large_image" />
+      <meta property="twitter:url" content={site} />
+      <meta property="twitter:title" content={title} />
+      <meta property="twitter:description" content={desc} />
+      <meta property="twitter:image" content="" />
       <link
         rel="apple-touch-icon"
         sizes="180x180"
@@ -32,7 +49,7 @@ export default function Header(Props: { title: string }): JSX.Element {
       <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
       <meta name="msapplication-TileColor" content="#2d89ef" />
       <meta name="theme-color" content="#ffffff" />
-      <link rel="canonical" href="https://gourav.io" />
+      <link rel="canonical" href={site} />
       <meta content="IE=edge" httpEquiv="X-UA-Compatible" />
       <meta name="robots" content="index,follow" />
       <meta name="googlebot" content="index,follow" />
