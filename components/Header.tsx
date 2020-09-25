91a118166c7848cad39647866828181651da74d4
jerrygoyal <1gouravgg@gmail.com>
2020-09-26 01:17:37 +0530

    added og img

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,6 +5,8 @@ export default function Header(Props: { title: string }): JSX.Element {
   const desc =
     "I'm a tech founder, full-stack developer, and a fun guy to hang around with.";
   const site = "https://gourav.io";
+  const image = `${site}/og.jpg`;
+  const handle = "@GorvGoyl";
   return (
     <Head>
       <title>{title}</title>
@@ -13,20 +15,27 @@ export default function Header(Props: { title: string }): JSX.Element {
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta name="title" content={title} />
       <meta name="description" content={desc} />
-
+      <meta name="image" content={image} />
+      {/* <!-- Schema.org for Google --> */}
+      <meta itemProp="name" content={title} />
+      <meta itemProp="description" content={desc} />
+      <meta itemProp="image" content={image} />
       {/* <!-- Open Graph / Facebook --> */}
       <meta property="og:type" content="website" />
+      <meta name="og:site_name" content={title} />
       <meta property="og:url" content={site} />
       <meta property="og:title" content={title} />
       <meta property="og:description" content={desc} />
-      <meta property="og:image" content={`${site}/og.jpg`} />
-
+      <meta property="og:image" content={image} />
       {/* <!-- Twitter --> */}
-      <meta property="twitter:card" content="summary_large_image" />
+      {/* content="summary" */}
+      <meta property="twitter:card" content="summary" />
       <meta property="twitter:url" content={site} />
       <meta property="twitter:title" content={title} />
       <meta property="twitter:description" content={desc} />
-      <meta property="twitter:image" content={`${site}/og.jpg`} />
+      <meta property="twitter:image" content={image} />
+      <meta name="twitter:site" content={handle} />
+      <meta name="twitter:creator" content={handle} />
       <link
         rel="apple-touch-icon"
         sizes="180x180"
@@ -44,7 +53,6 @@ export default function Header(Props: { title: string }): JSX.Element {
         sizes="16x16"
         href="/favicon-16x16.png"
       />
-
       <link rel="manifest" href="/site.webmanifest" />
       <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
       <meta name="msapplication-TileColor" content="#2d89ef" />
