2f2da9abbd25b28bd20acd14fb3a48947ff7f9a3
jerrygoyal <1gouravgg@gmail.com>
2020-09-26 00:03:57 +0530

    added meta tags

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,6 +6,36 @@ export default function Header(Props: { title: string }): JSX.Element {
       <title>{Props.title}</title>
       <meta charSet="UTF-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta
+        name="description"
+        content="I'm a tech founder, full-stack developer, and a fun guy to hang around with."
+      />
+      <link
+        rel="apple-touch-icon"
+        sizes="180x180"
+        href="/apple-touch-icon.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="32x32"
+        href="/favicon-32x32.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="16x16"
+        href="/favicon-16x16.png"
+      />
+
+      <link rel="manifest" href="/site.webmanifest" />
+      <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+      <meta name="msapplication-TileColor" content="#2d89ef" />
+      <meta name="theme-color" content="#ffffff" />
+      <link rel="canonical" href="https://gourav.io" />
+      <meta content="IE=edge" httpEquiv="X-UA-Compatible" />
+      <meta name="robots" content="index,follow" />
+      <meta name="googlebot" content="index,follow" />
     </Head>
   );
 }
