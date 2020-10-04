5aed224790e1cdc8b7dbc1b102dde8edac6f9adf
jerrygoyal <1gouravgg@gmail.com>
2020-10-04 23:38:45 +0530

    added notion ext redirects

---
+++ next.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  // Uncomment the line below to enable basePath, pages and
+  // redirects will then have a path prefix (`/app` in this case)
+  //
+  // basePath: '/app',
+
+  async redirects() {
+    return [
+      {
+        source: "/chrome",
+        destination:
+          "https://chrome.google.com/webstore/detail/notion-boost/eciepnnimnjaojlkcpdpcgbfkpcagahd",
+        permanent: true,
+      },
+      {
+        source: "/firefox",
+        destination:
+          "https://addons.mozilla.org/en-US/developers/addon/notion-boost/",
+        permanent: true,
+      },
+    ];
+  },
+};
