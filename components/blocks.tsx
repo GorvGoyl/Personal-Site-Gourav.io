d091833c121c5ae5da0c32ebe79ef4349a6157b9
jerrygoyal <1gouravgg@gmail.com>
2021-03-08 23:22:16 +0530

    added og

---
+++ components/blocks.tsx
@@ -0,0 +1,25 @@
+import Link from "next/link";
+import React from "react";
+import css from "./css/tags.module.scss";
+
+export function TwitterBtn(): JSX.Element {
+  return (
+    <div className="twitter mt-12  justify-center flex">
+      <div>
+        <a
+          href="https://twitter.com/GorvGoyl"
+          target="_blank"
+          className="twitter-btn"
+          title="Follow @GorvGoyl on Twitter"
+        >
+          <img
+            src="/twitter-light.svg"
+            className="inline w-4 h-4 m-0 mr-1"
+            alt="Twitter"
+          />
+          Follow @GorvGoyl
+        </a>
+      </div>
+    </div>
+  );
+}
