0db2e28bae17da91841af43116471fd49b822daf
Gourav <1gouravgg@gmail.com>
2021-04-02 16:38:48 +0530

    fixed safari date bug

--- lib/utils.ts
+++ lib/utils.ts
@@ -3,6 +3,7 @@ export const isProd = process.env.NODE_ENV === "production";
 // also replicate change in generate-rss.js
 export const webpackPath = "/_next/static/media/pages";
 
+// YYYY-MM-DD or YYYY-M-D
 export function readableDate(date: string): string {
   const dateFormatter = new Intl.DateTimeFormat("en-US", {
     month: "short",
@@ -10,7 +11,23 @@ export function readableDate(date: string): string {
     year: "numeric",
   });
   console.log("date: " + date);
-  const dateObj = new Date(date);
+
+  const YYYY = date.split("-")[0];
+
+  //convert M->MM i.e. 2->02
+  const MM =
+    date.split("-")[1].length == 1
+      ? "0" + date.split("-")[1]
+      : date.split("-")[1];
+
+  //convert D->DD i.e. 2->02
+  const DD =
+    date.split("-")[2].length == 1
+      ? "0" + date.split("-")[2]
+      : date.split("-")[2];
+
+  console.log("YYYY + MM + DD: " + YYYY + MM + DD);
+  const dateObj = new Date(YYYY + MM + DD);
   console.log("dateObj: " + dateObj);
   const dateString = dateFormatter.format(dateObj);
   console.log("dateString: " + dateString);
