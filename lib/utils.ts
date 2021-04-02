36b6e0af3cac5ae340160b651f893f7a26c45037
Gourav <1gouravgg@gmail.com>
2021-04-02 16:57:27 +0530

    fixed safari date bug

--- lib/utils.ts
+++ lib/utils.ts
@@ -5,36 +5,37 @@ export const webpackPath = "/_next/static/media/pages";
 
 // YYYY-MM-DD or YYYY-M-D
 export function readableDate(date: string): string {
-  const dateFormatter = new Intl.DateTimeFormat("en-US", {
-    month: "short",
-    day: "numeric",
-    year: "numeric",
-  });
-  console.log("date: " + date);
+  try {
+    const dateFormatter = new Intl.DateTimeFormat("en-US", {
+      month: "short",
+      day: "numeric",
+      year: "numeric",
+    });
 
-  const YYYY = date.split("-")[0];
+    const YYYY = date.split("-")[0];
 
-  //convert M->MM i.e. 2->02
-  const MM =
-    date.split("-")[1].length == 1
-      ? "0" + date.split("-")[1]
-      : date.split("-")[1];
+    //convert M->MM i.e. 2->02
+    const MM =
+      date.split("-")[1].length == 1
+        ? "0" + date.split("-")[1]
+        : date.split("-")[1];
 
-  //convert D->DD i.e. 2->02
-  const DD =
-    date.split("-")[2].length == 1
-      ? "0" + date.split("-")[2]
-      : date.split("-")[2];
+    //convert D->DD i.e. 2->02
+    const DD =
+      date.split("-")[2].length == 1
+        ? "0" + date.split("-")[2]
+        : date.split("-")[2];
 
-  // YYYY-MM-DD
-  const properDateString = `${YYYY + "-" + MM + "-" + DD}`;
+    // YYYY-MM-DD
+    const properDateString = `${YYYY + "-" + MM + "-" + DD}`;
 
-  console.log("YYYY-MM-DD" + properDateString);
-  const dateObj = new Date(properDateString);
+    const dateObj = new Date(properDateString);
 
-  console.log("dateObj: " + dateObj);
-  const dateString = dateFormatter.format(dateObj);
-  console.log("dateString: " + dateString);
+    const dateString = dateFormatter.format(dateObj);
 
-  return dateString;
+    return dateString;
+  } catch (e) {
+    console.log("Error in readableDate: " + JSON.stringify(e));
+    return "";
+  }
 }
