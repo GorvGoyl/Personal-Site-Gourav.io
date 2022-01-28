d5c8856b374d8382267ce0d9acd10771724055c5
gorvgoyl <1gouravgg@gmail.com>
2022-01-28 22:43:09 +0530

    misc

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,23 +1,21 @@
 export function Footer(): JSX.Element {
   return (
-    <div className="mt-10">
-      <div className="py-10 max-w-screen-md mx-auto border-t border-gray-300">
-        <footer className="">
-          <div className="text-center text-xs font-mono text-gray-600">
-            🖤 Built with React |{" "}
-            <a
-              target="_blank"
-              rel="noopener"
-              className="underline"
-              title="See source-code on Github"
-              href="https://github.com/GorvGoyl/Personal-Site-Gourav.io"
-            >
-              Open-source
-            </a>{" "}
-            🖤
-          </div>
-        </footer>
+    <footer className="mt-10 text-center font-mono text-gray-600">
+      • • •
+      <div className="pb-8 pt-6 md:pb-10 md:pt-7 max-w-screen-md mx-auto">
+        <div className=" text-xs ">
+          Built with React 🖤{" "}
+          <a
+            target="_blank"
+            rel="noopener"
+            className="underline"
+            title="See source-code on Github"
+            href="https://github.com/GorvGoyl/Personal-Site-Gourav.io"
+          >
+            Open-source
+          </a>{" "}
+        </div>
       </div>
-    </div>
+    </footer>
   );
 }
