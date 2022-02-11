80e70425640ca87bf0016cccfd706649632499e5
gorvgoyl <1gouravgg@gmail.com>
2022-02-11 18:46:46 +0530

    fun with console logs

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -14,6 +14,17 @@ const App = ({ Component, pageProps }: AppProps): JSX.Element => {
       if (isProd) gtag.pageview(url, document.title);
     };
     router.events.on("routeChangeComplete", handleRouteChange);
+    if (isProd) {
+      console.log(
+        "%cBuilt with Next.js (React), Typescript, Tailwind CSS | Blog content using Markdown (MDX) | Deployed on Vercel",
+        "color: #6af549; font-size: 14px; background: #282c34;"
+      );
+      console.log(
+        "%cSource-code: https://github.com/GorvGoyl/Personal-Site-Gourav.io",
+        "color: #6af549; font-size: 14px; background: #282c34;"
+      );
+    }
+
     return () => {
       router.events.off("routeChangeComplete", handleRouteChange);
     };
