3350480c112f0af83ab5bc32751e4c6f4a2166da
gorvgoyl <1gouravgg@gmail.com>
2022-03-27 17:31:58 +0530

    add en lang

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,7 +7,7 @@ import Document, { Head, Html, Main, NextScript } from "next/document";
 export default class MyDocument extends Document {
   render(): JSX.Element {
     return (
-      <Html>
+      <Html lang="en">
         <Head>
           <link
             rel="preload"
