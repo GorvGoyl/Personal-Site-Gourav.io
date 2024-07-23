926d527137b4b922f13d56ea7f346e1390f45450
gorvgoyl <1gouravgg@gmail.com>
2024-07-23 18:32:43 +0530

    fix layout

--- components/substack.tsx
+++ components/substack.tsx
@@ -3,7 +3,7 @@ export function SubstackForm() {
         <div className="flex flex-row items-center justify-center">
             <div className="relative flex w-fit flex-col items-center justify-center rounded-2xl border-2 border-orange-400 px-5 py-2">
                 <div className="mt-4 text-xl font-bold">Gourav's Newsletter</div>
-                <div className="mt-3 text-base text-slate-500">
+                <div className="mb-1 mt-3 text-base text-slate-500">
                     I write about startups, tech, productivity, personal development, and more.
                 </div>
                 <iframe
@@ -17,9 +17,9 @@ export function SubstackForm() {
                     frameBorder="0"
                     scrolling="no"
                 />
-                <div className="pointer-events-none absolute bottom-[40px] z-50 h-[45px] w-[450px] select-none bg-white pt-3 text-center text-xs text-slate-400">
+                {/* <div className="pointer-events-none absolute bottom-[40px] z-50 h-[45px] w-[450px] select-none bg-white pt-3 text-center text-xs text-slate-400">
                     No Spam, Ever.
-                </div>
+                </div> */}
             </div>
         </div>
     );
