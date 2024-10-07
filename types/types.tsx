7907566887f4f8604fe3bf895c6486fb56a71621
gorvgoyl <1gouravgg@gmail.com>
2024-10-07 14:28:54 +0300

    fix slug

--- types/types.tsx
+++ types/types.tsx
@@ -1,33 +1,33 @@
-import { z } from "zod";
+import { z } from 'zod';
 
 export const FrontmatterBlogpostSchema = z.object({
-  date: z.string().nullable(),
-  title: z.string().trim().min(3),
-  desc: z.string().trim(),
-  slug: z.string().min(3), //don't trim/modify slug as it is used to fetch page id again
-  postId: z.string().min(3),
-  preview: z.boolean(),
-  published: z.boolean(),
-  desktopOutline: z.boolean(),
-  mobileOutline: z.boolean(),
-  ogImage: z.string().optional(),
-  twitter: z.string().optional(),
-  linkedin: z.string().optional(),
-  hackernews: z.string().optional(),
-  reddit: z.string().optional(),
-  github: z.string().optional(),
+    date: z.string().nullable(),
+    title: z.string().trim().min(3),
+    desc: z.string().trim(),
+    slug: z.string().min(2), //don't trim/modify slug as it is used to fetch page id again
+    postId: z.string().min(3),
+    preview: z.boolean(),
+    published: z.boolean(),
+    desktopOutline: z.boolean(),
+    mobileOutline: z.boolean(),
+    ogImage: z.string().optional(),
+    twitter: z.string().optional(),
+    linkedin: z.string().optional(),
+    hackernews: z.string().optional(),
+    reddit: z.string().optional(),
+    github: z.string().optional(),
 });
 
 export type FrontmatterBlogpost = z.infer<typeof FrontmatterBlogpostSchema>;
 
 export const FrontmatterProjectSchema = z.object({
-  title: z.string().trim().min(3),
-  desc: z.string().trim(),
-  slug: z.string().min(3),
-  ogImgURL: z.string().min(3),
-  preview: z.boolean(),
-  desktopOutline: z.boolean(),
-  mobileOutline: z.boolean(),
+    title: z.string().trim().min(3),
+    desc: z.string().trim(),
+    slug: z.string().min(2),
+    ogImgURL: z.string().min(3),
+    preview: z.boolean(),
+    desktopOutline: z.boolean(),
+    mobileOutline: z.boolean(),
 });
 
 export type FrontmatterProject = z.infer<typeof FrontmatterProjectSchema>;
