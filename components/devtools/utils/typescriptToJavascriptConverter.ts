1ec42b86780f9c774d53a3a082d338cada3a013c
gorvgoyl <1gouravgg@gmail.com>
2024-11-30 16:39:40 +0700

    chore

--- components/devtools/utils/typescriptToJavascriptConverter.ts
+++ components/devtools/utils/typescriptToJavascriptConverter.ts
@@ -4,7 +4,7 @@ export function convertTypeScriptToJavaScript(tsCode: string): string {
     const result = ts.transpileModule(tsCode, {
         compilerOptions: {
             module: ts.ModuleKind.CommonJS,
-            target: ts.ScriptTarget.ES2015,
+            target: ts.ScriptTarget.ES2020,
         },
     });
     return result.outputText;
