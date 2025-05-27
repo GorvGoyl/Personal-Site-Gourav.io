e99579cf4f5eaf033c2cbe2995ff8add0965cfd2
gorvgoyl <1gouravgg@gmail.com>
2025-05-27 23:58:23 +0700

    chore

--- components/devtools/TimezoneConverter.tsx
+++ components/devtools/TimezoneConverter.tsx
@@ -172,7 +172,7 @@ export function TimezoneConverter() {
                 onClose={() => {
                     setIsOpen(false);
                 }}>
-                <div className="rounded-lg border border-neutral-200 p-5">
+                <div className="w-[300] rounded-lg border border-neutral-200 p-5 md:w-auto">
                     <h2 className="mb-4 text-xl font-semibold">Timezone Converter</h2>
 
                     <div className="mb-4">
